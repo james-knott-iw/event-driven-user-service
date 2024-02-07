e5f7a2063e6ff57aca61febb4b5c3d504a9fab97
james-knott-iw <james.knott@integration.works>
2024-02-08 11:54:53 +1300

    add publisher and subscriber to docker compose

--- publisher/src/main/java/works/integration/userservice/SpringBootPublisher.java
+++ publisher/src/main/java/works/integration/userservice/SpringBootPublisher.java
@@ -1,6 +1,12 @@
 package works.integration.userservice;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.jms.Connection;
+import jakarta.jms.JMSException;
+import jakarta.jms.MessageProducer;
+import jakarta.jms.Session;
+import jakarta.jms.Topic;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +15,10 @@ import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import com.solacesystems.jms.SolConnectionFactory;
+import com.solacesystems.jms.SolJmsUtility;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +34,7 @@ public class SpringBootPublisher {
 	private JmsTemplate jmsTemplate;
 
 	@PostConstruct
-	private void customizeJmsTemplate() {
+	private void customizeJmsTemplate() throws JMSException {
 		// Update the jmsTemplate's connection factory to cache the connection
 		CachingConnectionFactory ccf = new CachingConnectionFactory();
 		ccf.setTargetConnectionFactory(jmsTemplate.getConnectionFactory());
@@ -33,6 +43,26 @@ public class SpringBootPublisher {
 		// By default Spring Integration uses Queues, but if you set this to true you
 		// will send to a PubSub+ topic destination
 		jmsTemplate.setPubSubDomain(true);
+		// Create Solace topic
+		createSolaceTopic();
+	}
+
+	private void createSolaceTopic() throws JMSException {
+		Connection connection = null;
+		Session session = null;
+		try {
+			connection = jmsTemplate.getConnectionFactory().createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic topic = session.createTopic(topicName);
+			MessageProducer producer = session.createProducer(topic);
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
 	}
 
 	@Value("${topic.name}")
